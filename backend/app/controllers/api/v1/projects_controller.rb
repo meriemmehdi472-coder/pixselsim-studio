52ef1314b64b439554871e57cb85c3fd3abcc3ea
meriemmehdi472-coder <meriemmehdi472@gmail.com>
2026-03-04 12:01:01 +0100

    database

---
+++ backend/app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class ProjectsController < ApplicationController
+      def index
+        @projects = Project.all
+        render json: @projects
+      end
+
+      def show
+        @project = Project.find(params[:id])
+        render json: @project
+      end
+
+      def create
+        @project = Project.new(project_params)
+        if @project.save
+          render json: @project, status: :created
+        else
+          render json: @project.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @project = Project.find(params[:id])
+        @project.destroy
+        render json: { message: 'Projet supprimé' }
+      end
+
+      private
+
+      def project_params
+        params.require(:project).permit(:title, :description)
+      end
+    end
+  end
+end
