37ddf9a9bcf352f1d9ccd7bc8fb9edb6dc9e5a93
meriemmehdi472-coder <meriemmehdi472@gmail.com>
2026-03-05 04:12:38 +0100

    fixer le build script

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -3,11 +3,12 @@ set -o errexit
 
 # Installer ffmpeg
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o /tmp/ffmpeg.tar.xz
-mkdir -p /tmp/ffmpeg
-tar -xf /tmp/ffmpeg.tar.xz -C /tmp/ffmpeg --strip-components=1
-cp /tmp/ffmpeg/ffmpeg /opt/render/project/bin/ffmpeg
-cp /tmp/ffmpeg/ffprobe /opt/render/project/bin/ffprobe
-export PATH="/opt/render/project/bin:$PATH"
+mkdir -p /tmp/ffmpeg-bin
+tar -xf /tmp/ffmpeg.tar.xz -C /tmp/ffmpeg-bin --strip-components=1
+mkdir -p $HOME/bin
+cp /tmp/ffmpeg-bin/ffmpeg $HOME/bin/ffmpeg
+cp /tmp/ffmpeg-bin/ffprobe $HOME/bin/ffprobe
+export PATH="$HOME/bin:$PATH"
 
 bundle install
 bundle exec rails db:migrate
