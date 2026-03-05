9c02c26799cd1f90cc22c282beda9162859e28a1
meriemmehdi472-coder <meriemmehdi472@gmail.com>
2026-03-05 08:44:21 +0100

    doublant rem

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,10 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173",
+            "http://localhost:3000",
             "https://pixselsim-studio-front.onrender.com"
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ["Authorization"],
+      max_age: 600
   end
 end
