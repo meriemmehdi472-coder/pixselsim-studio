9ddf80c2d67df91770204e88a35f911148734979
meriemmehdi472-coder <meriemmehdi472@gmail.com>
2026-03-05 03:12:12 +0100

    ajourt d'url cors.rb

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173",
+            "https://pixselsim-studio-front.onrender.com"
 
     resource "*",
       headers: :any,
