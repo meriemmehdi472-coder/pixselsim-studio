cdda29a0b1c6285b79a04b5eb0a21c535b02e821
meriemmehdi472-coder <meriemmehdi472@gmail.com>
2026-03-05 19:56:26 +0100

    +

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://pixelsim-studio-front.onrender.com",
+    origins "https://pixselsim-studio-front.onrender.com",
             "http://localhost:5173",
             "http://localhost:3000"
 
