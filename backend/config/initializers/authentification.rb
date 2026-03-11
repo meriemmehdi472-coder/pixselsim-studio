988dbcd57952ab3fb4bcedd2da1816e61f8997dc
meriemmehdi472-coder <meriemmehdi472@gmail.com>
2026-03-11 23:34:23 +0100

    fix omniauth config devise

--- backend/config/initializers/authentification.rb
+++ backend/config/initializers/authentification.rb
@@ -11,14 +11,3 @@ Rails.application.config.middleware.use ActionDispatch::Session::CookieStore,
 OmniAuth.config.allowed_request_methods = [:get, :post]
 OmniAuth.config.silence_get_warning     = true
 OmniAuth.config.full_host = ENV.fetch("BACKEND_URL", "http://localhost:3000")
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-  ENV["GOOGLE_CLIENT_ID"] || Rails.application.credentials.dig(:google, :client_id),
-  ENV["GOOGLE_CLIENT_SECRET"] || Rails.application.credentials.dig(:google, :client_secret),  
-  {
-      scope:         "email,profile",
-      prompt:        "select_account",
-      callback_path: "/auth/google_oauth2/callback"
-    }
-end
