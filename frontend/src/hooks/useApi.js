84ce0b2404ad2c6630b98ba45074b51ba1b6da02
meriemmehdi472-coder <meriemmehdi472@gmail.com>
2026-03-13 01:04:19 +0100

    jwt UseApi

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -3,6 +3,16 @@ const API = (import.meta.env.VITE_API_URL || "http://localhost:3000") + "/api/v1
 export function useApi() {
   const request = async (path, options = {}) => {
     const isFormData = options.body instanceof FormData;
+
+    const token = localStorage.getItem("auth_token");
+
+    const headers = isFormData
+      ? {}
+      : { "Content-Type": "application/json" };
+
+    if (token) headers["Authorization"] = `Bearer ${token}`;
+
+
     const res = await fetch(`${API}${path}`, {
       credentials: "include",
       headers: isFormData ? {} : { "Content-Type": "application/json" },
